37280fd339e8273e5e2b5f1d2e158986cfbcb3d3
Jun Heng <zudoturiku@duck.com>
2022-07-10 19:03:33 +0800

    Initial Commit

---
+++ Speak/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speak
+//
+//  Created by Wong Jun heng on 10/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Speak/SpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApp.swift
+//  Speak
+//
+//  Created by Wong Jun heng on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
