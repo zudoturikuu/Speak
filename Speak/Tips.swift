2f038a03fc2ebc842d1b428bb5778c7a4a78a5b1
Jun Heng <zudoturiku@duck.com>
2022-07-27 22:18:43 +0800

    Added tips struct

---
+++ Speak/Tips.swift
@@ -0,0 +1,13 @@
+//
+//  Tips.swift
+//  Speak
+//
+//  Created by Wong Jun heng on 27/7/22.
+//
+
+import Foundation
+
+struct Tips: Identifiable, Codable {
+    var id = UUID()
+    var title: String
+}
