94c35e10b37b49d8c99d329df51136db1a3afa0f
Jun Heng <zudoturiku@duck.com>
2022-08-15 15:08:22 +0800

    idk

--- Speak/TipsView.swift
+++ Speak/TipsView.swift
@@ -12,14 +12,32 @@ struct TipsView: View {
     @ObservedObject var tipManager: TipManager
     
     var body: some View {
-        ScrollView {
-            VStack {
-                
+        NavigationView {
+            VStack (alignment: .leading) {
+                ScrollView {
+                    VStack (alignment: .leading) {
+                        Text("Browse through our hundreds of Social Etiquette tips")
+                            .font(.system(.headline, design: .rounded))
+                            .foregroundStyle(.primary)
+                            .padding(.horizontal)
+                            .padding(.top)
+                        
+                        ScrollView {
+                            HStack {
+                                ForEach(Tip) {
+                                    tips in
+                                    
+                                }
+                            }
+                        }
+                    }
+                    
+                }
             }
+        }
+        .navigationTitle("Social Etiquette Tips")
 }
-    }
 }
-
 struct TipsView_Previews: PreviewProvider {
     static var previews: some View {
         TipsView(tipManager: TipManager())
